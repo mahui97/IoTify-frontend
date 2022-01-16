d49437b4789d1925ca13f8a21bf3463b88e142e9
麻慧 <mh19@mails.tsinghua.edu.cn>
2022-01-16 12:14:12 +0800

    ocrcode

--- src/common/js/api.js
+++ src/common/js/api.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import axios from 'axios'
 
 let http = axios.create({
-    baseURL: 'http://62.234.44.237:8888',
-    // baseURL: 'http://192.168.43.232:8888',
+    // baseURL: 'http://62.234.44.237:8888',
+    baseURL: 'http://192.168.31.235:8888',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import * as echarts from 'echarts'
 import router from './router'
 import Api from './common/js/api.js'
 import {formatDate} from './common/js/date.js'
 import global_ from './include/Global'
 Vue.prototype.$api = Api;
+Vue.prototype.$echarts = echarts;
 Vue.prototype.GLOBAL = global_;
 
 Vue.config.productionTip = false
